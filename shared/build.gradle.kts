0add46d3b35f53b8cdabde3c963f688d7dd856b5
YagiRyu <ryutaro822@icloud.com>
2022-06-03 14:41:29 +0900

    依存関係をプロジェクトに追加した。

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -2,6 +2,8 @@ plugins {
     kotlin("multiplatform")
     kotlin("native.cocoapods")
     id("com.android.library")
+    id("com.squareup.sqldelight")
+    kotlin("plugin.serialization")
 }
 
 version = "1.0"
@@ -23,13 +25,29 @@ kotlin {
     }
     
     sourceSets {
-        val commonMain by getting
+        val commonMain by getting {
+            dependencies {
+                implementation(libs.ktor.core)
+                implementation(libs.ktor.logging)
+                implementation(libs.kotlinx.coroutines.core)
+                implementation(libs.napier)
+                implementation(libs.kotlinx.serialization.json)
+                implementation(libs.koin.core)
+                implementation(libs.multiplatform.settings)
+                implementation(libs.sqldelight.coroutines.extension)
+            }
+        }
         val commonTest by getting {
             dependencies {
                 implementation(kotlin("test"))
             }
         }
-        val androidMain by getting
+        val androidMain by getting {
+            dependencies {
+                implementation(libs.ktor.client.okhttp)
+                implementation(libs.sqldelight.android.driver)
+            }
+        }
         val androidTest by getting
         val iosX64Main by getting
         val iosArm64Main by getting
@@ -39,6 +57,10 @@ kotlin {
             iosX64Main.dependsOn(this)
             iosArm64Main.dependsOn(this)
             iosSimulatorArm64Main.dependsOn(this)
+            dependencies {
+                implementation(libs.ktor.client.ios)
+                implementation(libs.sqldelight.native.driver)
+            }
         }
         val iosX64Test by getting
         val iosArm64Test by getting
